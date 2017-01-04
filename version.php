3d807558fe882d211ca9406a1ac7f5e933af37e7
Richard Lobb <richard.lobb@canterbury.ac.nz>
2017-01-04 20:17:36 +1300

    Update version number.

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ $plugin->requires = 2013051400;
 $plugin->cron = 0;
 $plugin->component = 'qbehaviour_adaptive_adapted_for_coderunner';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.3';
+$plugin->release = '1.3.1';
 
 $plugin->dependencies = array();
