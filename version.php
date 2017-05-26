2f8f8901a0f89cf8e04f51bed1a6ad8deee02173
Richard Lobb <richard.lobb@canterbury.ac.nz>
2017-05-26 13:16:39 +1200

    Update version number.

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2017030200;
+$plugin->version  = 2017052600;
 $plugin->requires = 2013051400;
 $plugin->cron = 0;
 $plugin->component = 'qbehaviour_adaptive_adapted_for_coderunner';
